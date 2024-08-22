0c3ef6eae31e5dcdae4cad9b153068d624c2fac9
kevinxzy1006 <161264609+kevinxzy1006@users.noreply.github.com>
2024-08-22 17:58:52 +0930

    main-1-1.cpp

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
 #include "MusicBox.h"
+#include <iostream>
 
-int main()
-{
-    MusicBox musicbox;
-    musicbox.get_song();
+int main() {
+    // Test default constructor
+    MusicBox box1;
+    std::cout << "Default MusicBox song: " << box1.get_song() << std::endl;
+    std::cout << "Default MusicBox width: " << box1.get_width() << " cm" << std::endl;
 
-    musicbox.get_width();
+    // Test parameterized constructor
+    MusicBox box2("Fur Elise", 15);
+    std::cout << "Custom MusicBox song: " << box2.get_song() << std::endl;
+    std::cout << "Custom MusicBox width: " << box2.get_width() << " cm" << std::endl;
 
     return 0;
 }
